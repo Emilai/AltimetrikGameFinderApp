aac742ace6eb61fe83d5ba3aaec63d18d98d5983
Emiliano Laino <nucleo.avi@gmail.com>
2021-08-01 10:03:55 -0300

    corrected indentation of files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 //////////////// Password Show Hide
 
 function password_show_hide() {
@@ -22,7 +21,7 @@ function password_show_hide() {
 /////////////////Carousel (Bootstrap)
 
 $('.carousel').carousel({
-interval: 2000
+  interval: 2000
 })
 
 ///////////////////////////////////////////////////
@@ -31,7 +30,7 @@ interval: 2000
 
 var snackbarError = document.getElementById("snackbarError");
 
-function snackbarClose() {  
+function snackbarClose() {
   snackbarError.classList.add("d-none");
 }
 
@@ -43,10 +42,10 @@ function snackbarClose() {
 const loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', function (e) {
-  
-  
+
+
   e.preventDefault();
-  
+
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   var snackbarError = document.getElementById("snackbarError");
@@ -60,44 +59,44 @@ loginForm.addEventListener('submit', function (e) {
   var eyeField = document.getElementById("eyeField");
 
 
-fetch("http://localhost:3000/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      "email": email,
-      "password": password,
-    }),
-  })
+  fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password,
+      }),
+    })
 
-  .then(function(response){
-    console.log(response.ok);
+    .then(function (response) {
+      console.log(response.ok);
 
-    if (response.ok === true){
+      if (response.ok === true) {
 
-    snackbarSuccess.classList.remove("d-none");
-    window.location.href="main.html";
+        snackbarSuccess.classList.remove("d-none");
+        window.location.href = "main.html";
 
-  } else if (user.checkValidity() == false){
+      } else if (user.checkValidity() == false) {
 
-    user.classList.add("is-invalid");
-    userIcon.classList.add("is-invalid-icon");
-    userIconField.classList.add("is-invalid");
+        user.classList.add("is-invalid");
+        userIcon.classList.add("is-invalid-icon");
+        userIconField.classList.add("is-invalid");
 
-  } else if (password.length < 4) {
+      } else if (password.length < 4) {
 
-    pass.classList.add("is-invalid");
-    keyField.classList.add("is-invalid");
-    keyIcon.classList.add("is-invalid-icon");
-    eyeField.classList.add("is-invalid");
+        pass.classList.add("is-invalid");
+        keyField.classList.add("is-invalid");
+        keyIcon.classList.add("is-invalid-icon");
+        eyeField.classList.add("is-invalid");
 
-  } else {
-    snackbarError.classList.remove("d-none");
- 
-  }
-  })
+      } else {
+        snackbarError.classList.remove("d-none");
+
+      }
+    })
 
 })
 
